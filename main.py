7a373e0e6d333ebca66a5c67e5146f0cd1bd24ea
sergeP70 <serge@pille.be>
2025-01-05 10:18:53 +0100

    optimizing code

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ import glob
 from pathlib import Path
 from fpdf import FPDF
 
-filepath = glob.glob("files/*")
+filepaths = glob.glob("files/*.txt")
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
-for file in filepath:
-    filename = Path(file).stem
-    print(filename)
+for filepath in filepaths:
+    filename = Path(filepath).stem.title()
     pdf.add_page()
 
     # Header
     pdf.set_font(family='Times', style='B', size=16)
     pdf.cell(w=0, h=12, text=filename, align='L', new_x='LMARGIN', new_y='NEXT')
-
 
 pdf.output("output/animals.pdf")
 
