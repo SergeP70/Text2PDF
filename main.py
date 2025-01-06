c4c90574c00c593680d6f7948060e94caa4e53a0
sergeP70 <serge@pille.be>
2025-01-06 07:51:03 +0100

    Adding the content of the text file

--- main.py
+++ main.py
@@ -3,19 +3,26 @@ import glob
 from pathlib import Path
 from fpdf import FPDF
 
+
 filepaths = glob.glob("files/*.txt")
 pdf = FPDF(orientation='P', unit='mm', format='A4')
 
 for filepath in filepaths:
+    # Get the filename without the extension
     filename = Path(filepath).stem
+
+    # Get the content of the text file
+    with open(filepath, 'r') as file:
+        content = file.read()
+
     pdf.add_page()
 
     # Header
     pdf.set_font(family='Times', style='B', size=16)
-    pdf.cell(w=0, h=12, text=filename.title(), align='L', new_x='LMARGIN', new_y='NEXT')
+    pdf.cell(w=0, h=16, text=filename.title(), align='L', new_x='LMARGIN', new_y='NEXT')
+
+    # Body
+    pdf.set_font(family="Times", size=12)
+    pdf.multi_cell(w=0, h=6, text=content)
 
 pdf.output("output/animals.pdf")
-
-
-
-
